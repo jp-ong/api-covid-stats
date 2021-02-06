457d5fcecbc4b1f5b184a71c6ef2727d1357833e
John Paul Ong <jpong5202@gmail.com>
2021-02-06 17:24:23 +0800

    Removed country_codes and made DRY

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -5,6 +5,14 @@ const Stat = require("../../models/Stat");
 
 const connectDB = require("../../config/db");
 
+const STAT_PROJECT = {
+  country_iso2s: 0,
+  country_iso3s: 0,
+  combined_names: 0,
+  uids: 0,
+  country_codes: 0,
+};
+
 router.get("/latest/:days", async (req, res) => {
   await connectDB();
 
@@ -15,12 +23,6 @@ router.get("/latest/:days", async (req, res) => {
       ).setUTCHours(0, 0, 0, 0)
     );
     const QUERY = { $and: [{ date }, { country_codes: { $ne: [] } }] };
-    const STAT_PROJECT = {
-      country_iso2s: 0,
-      country_iso3s: 0,
-      combined_names: 0,
-      uids: 0,
-    };
     const OPTIONS = { sort: { country: 1 } };
 
     const stats = await Stat.find(QUERY, STAT_PROJECT, OPTIONS);
@@ -39,12 +41,6 @@ router.get("/country/:country", async (req, res) => {
   try {
     const QUERY = {
       $and: [{ country: req.params.country }, { country_codes: { $ne: [] } }],
-    };
-    const STAT_PROJECT = {
-      country_iso2s: 0,
-      country_iso3s: 0,
-      combined_names: 0,
-      uids: 0,
     };
     const OPTIONS = { sort: { date: -1 } };
 
